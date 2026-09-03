c05f6e4a225ec8c31647e9eac5fe2eac5197b0db
wuhao <wuhao01@loongson.cn>
2026-09-03 09:18:39 +0000

    Add LoongArch64 cross-build CI

---
+++ .github/toolchains/gcc-loongarch64-linux-gnu.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR loongarch64)
+
+set(CMAKE_C_COMPILER loongarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER loongarch64-linux-gnu-g++)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
